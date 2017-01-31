f4eca4c79ed7cf68620325de08408123437f7a7f
petrvanblokland <buro@petr.com>
2017-01-31 11:40:02 +0100

    Start of PostIdentity
    Add _export to ignore.

--- src/PosterIdentity/PosterIdentity.py
+++ src/PosterIdentity/PosterIdentity.py
@@ -1,5 +1,5 @@
 # 
-#    Example responsive FaceBook Banner
+#    Example Poster Identity
 #
 # Create a RootStyle instance by calling function. Alter some of the default values
 # on initialization, so we don't that to replace them later for our version of the 
@@ -10,27 +10,21 @@ from pagebot.style import getRootStyle
 from pagebot.document import Document
 from pagebot.page import Template
 
-def mm(v):
-    return int(round(v*72/25.4))
-
-SHOW_GRID = True
-SHOW_GRID_COLUMNS = True
+SHOW_GRID = False
+SHOW_GRID_COLUMNS = False
 SHOW_BASELINE_GRID = False
 
+W = 520
+H = 700
 
 RS = getRootStyle(
-    w=851, h=313, # Default 851 x 313
+    w=W, h=H, 
     showGrid=SHOW_GRID, 
     showGridColumns=SHOW_GRID_COLUMNS,
     showBaselineGrid = SHOW_BASELINE_GRID,
-    )
-FONT_SIZE = 24
-RS['fontSize'] = FONT_SIZE
-RS['baselineGrid'] = FONT_SIZE * 1.2
-RS['docW'] = RS['w'] + 200
-RS['docH'] = RS['h'] + 200
-
-RED = (1, 0, 0, 0.5)
+)
+RS['docW'] = W + 200
+RS['docH'] = H + 200
 
 def makeDocument():
 
@@ -39,37 +33,14 @@ def makeDocument():
     # Show grid columns and margins if rootStyle.showGrid or rootStyle.showGridColumns are True
     template.grid(RS) 
     # Show baseline grid if rs.showBaselineGrid is True
-    template.baselineGrid(RS)
-    cb = 3
-    template.cRect(1, 0, cb, 1, RS, fill=(1, 0, 0))
-    template.cRect(1+cb, 0, 3, 1, RS, fill=(0, 0, 0.5))
     # Create empty image place holders. To be filled by running content on the page.
     #template.cContainer(2, -0.7, 5, 4, rs)  # Empty image element, cx, cy, cw, ch
     # Create new document with (w,h) and fixed amount of pages.
     # Make number of pages with default document size.
     # Initially make all pages default with template2
-    doc = Document(RS, pages=3, template=template) 
-    
-    doc.newStyle(name='title', fontSize=32, font='Verdana', textColor=RED)
-
-    page = doc.pages[2]
-    #page.style['w'] = 400
-    #page.style['h'] = 200
-    page.cRect(1, 1, cb, 1, RS, fill=(0, 0.5, 0))
-    
-    # For all pages in the document, add page number.
-    for pageNumber in doc.pages:
-        page = doc.pages[pageNumber] # Get this page
-        # Add box with page number
-        # TODO: Vertical page index is reversed direction now.
-        page.cText('Page %s' % pageNumber, 8, 2, 1, 1, RS, 
-            textColor=0, fontSize=32)
-        # TODO: Apply style direct on this function.
-        page.cText('Hallo', 4, 1, cb, 1, RS, textColor=RED)
-    
-    return doc 
-    
+    return Document(RS, pages=1, template=template) 
+        
 d = makeDocument()
-d.export('export/FaceBookBanner.pdf')
+d.export('_export/PosterIdentity.pdf')
 
 
