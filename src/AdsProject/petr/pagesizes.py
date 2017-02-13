0d8590ee6bcecdae145034bf0568f6680e712f98
petrvanblokland <buro@petr.com>
2017-02-13 14:55:46 +0100

    Latest version

--- src/AdsProject/petr/pagesizes.py
+++ src/AdsProject/petr/pagesizes.py
@@ -1,5 +1,5 @@
 from pagebot.style import MM
 
-PageSizes = {
-    'Spread Bleed': (301*MM, 424*MM),    'Full Page Bleed': (214*MM, 301*MM),    'Full Page A4': (210*MM, 297*MM),    'Horizontal 2/3': (145,6m*MM, 184*MM),    'Horizontal 1/3': (77.8*MM, 184*MM),    'Horizontal 1/2': (123.3*MM, 85.5*MM),    'Horizontal 1/4': (55.1*MM, 184*MM),    'Vertical 1/2': (259.6*MM, 85.5*MM),    'Vertical 1/4': (123.3*MM, 85,5*MM),    'Horizontal 1/8': (55.1*MM, 85.5*MM),    'VogueMagazine': (220*MM, 285*MM),
+pageSizes = {
+    'SpreadBleed': (424*MM, 301*MM),    'FullPageBleed': (214*MM, 301*MM),    'FullPageA4': (210*MM, 297*MM),    'Horizontal2_3': (145.6*MM, 184*MM),    'Horizontal1_3': (77.8*MM, 184*MM),    'Horizontal1_2': (123.3*MM, 85.5*MM),    'Horizontal1_4': (55.1*MM, 184*MM),    'Vertical1_2': (259.6*MM, 85.5*MM),    'Vertical1_4': (123.3*MM, 85.5*MM),    'Horizontal1_8': (55.1*MM, 85.5*MM),    'VogueMagazine': (220*MM, 285*MM),
 }
